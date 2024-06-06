a11ea3d19c4f5959310564de16877c17a689773e
Stephane Torres <stephane.torres@madetech.com>
2024-06-06 10:47:51 +0100

    test:GREEN get_random_word first element is a string

--- tests/test_random_word.py
+++ tests/test_random_word.py
@@ -4,4 +4,8 @@ def test_call_random_word_api():
     assert type(get_random_word(5)) == list
 
 def test_word_length_is_equal_to_five():
-    assert len(get_random_word(5)[0]) == 5 
+    assert len(get_random_word(5)[0]) == 5
+
+def test_word_is_type_string():
+    assert type(get_random_word(6)[0]) == str
+    assert len(get_random_word(6)[0]) == 6 
