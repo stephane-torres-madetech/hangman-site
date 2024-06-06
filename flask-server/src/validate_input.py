5791a167e3616a091f8051953448a03bf06716f7
Stephane Torres <stephane.torres@madetech.com>
2024-06-06 14:20:03 +0100

    only single char string valid input, type safety on validate function

--- flask-server/src/validate_input.py
+++ flask-server/src/validate_input.py
@@ -1,2 +1,2 @@
-def validate_input(input):
-    return type(input) == str
+def validate_input(input: str, guessed_letters: list[str]):
+    return type(input) == str and input not in guessed_letters and len(input) == 1
--- flask-server/tests/test_player_input.py
+++ flask-server/tests/test_player_input.py
@@ -1,7 +1,13 @@
 from src.validate_input import validate_input
 
 def test_int_is_not_valid():
-    assert validate_input(12) == False
+    assert validate_input(12, []) == False
 
 def test_one_letter_is_valid_input():
-    assert validate_input('e') == True
+    assert validate_input('e', []) == True
+
+def test_multiple_letter_invalid():
+    assert validate_input('ef', []) == False
+
+def test_already_guessed_letters_are_invalid():
+    assert validate_input('e', ['e']) == False
