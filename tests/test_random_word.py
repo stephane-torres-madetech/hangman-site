01900329ead47a83a98eb70704b85af1984ab644
Stephane Torres <stephane.torres@madetech.com>
2024-06-06 10:32:41 +0100

    test:GREEN, successful api call with word of correct length

--- tests/test_random_word.py
+++ tests/test_random_word.py
@@ -1,7 +1,7 @@
 from ..get_random_word import get_random_word
 
 def test_call_random_word_api():
-    assert type(get_random_word()) == list
+    assert type(get_random_word(5)) == list
 
 def test_word_length_is_equal_to_five():
     assert len(get_random_word(5)[0]) == 5 
