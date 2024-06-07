e8717716ffe302d587cf9f9a7ee8442a312b1eb8
Stephane Torres <stephane.torres@madetech.com>
2024-06-07 11:33:45 +0100

    slowing adding in the gallows

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UsersGuess from './components/UsersGuess'
+import Hangman from './components/hangman'
 
 
 // server running on local host 5000
@@ -11,29 +12,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-          
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-          
-        </p>
+     
        <UsersGuess/>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       
+    
+      
+      <Hangman/>
     </>
   )
 }
--- front-end/src/components/hangman.jsx
+++ front-end/src/components/hangman.jsx
@@ -1,18 +1,17 @@
-function Hangman(){
-
-
-    return (
-        <div className="hangman-container">
-            <div className="gallows">
-                <div className="top"></div>
-                <div className="rope"></div>
-                <div className="frame"></div>
-                <div className="base"></div>
-
-            </div>
-            <div className="hangman">
-                <div className="head"></div>
-            </div>
-        </div>
-    )
+function Hangman() {
+  return (
+    <div className="z-10">
+      <div className="">
+        <div className=""></div>
+        <div className="w-90 bg-black ml-16">hello where am i</div>
+        <div className="w-6 h-96 bg-black ml-16"></div>
+        <div className="w-96 bg-black ">Base</div>
+      </div>
+      <div className="hangman">
+        <div className="head"></div>
+      </div>
+    </div>
+  );
 }
+
+export default Hangman;
