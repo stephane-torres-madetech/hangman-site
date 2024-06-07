7e394e367c1d96bed8c9468060943034cf375099
Stephane Torres <stephane.torres@madetech.com>
2024-06-07 11:18:31 +0100

    giving tailwind a go, to hopefully componenet inline styling easier

---
+++ front-end/src/components/hangman.jsx
@@ -0,0 +1,18 @@
+function Hangman(){
+
+
+    return (
+        <div className="hangman-container">
+            <div className="gallows">
+                <div className="top"></div>
+                <div className="rope"></div>
+                <div className="frame"></div>
+                <div className="base"></div>
+
+            </div>
+            <div className="hangman">
+                <div className="head"></div>
+            </div>
+        </div>
+    )
+}
