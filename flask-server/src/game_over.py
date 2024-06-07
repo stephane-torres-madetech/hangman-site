c9a4ed953c5527ea46d2befdbcd2e04d8ee36e5c
Stephane Torres <stephane.torres@madetech.com>
2024-06-07 10:26:09 +0100

    test:GREEN game over depends on number of guesses

--- flask-server/src/game_over.py
+++ flask-server/src/game_over.py
@@ -1,2 +1,2 @@
-def game_over():
-    return True
+def game_over(number_of_guesses):
+    return number_of_guesses == 6
--- flask-server/tests/test_game_over.py
+++ flask-server/tests/test_game_over.py
@@ -1,7 +1,7 @@
 from src.game_over import game_over
 
 def test_game_over_too_many_guesses():
-    assert game_over() == True
+    assert game_over(6) == True
 
 def test_game_not_over_fewer_than_max_guesses():
-    assert game_over() == False
+    assert game_over(5) == False
