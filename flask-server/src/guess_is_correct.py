07753b1ebac0c9f7ee355c15170541343c14a28d
Stephane Torres <stephane.torres@madetech.com>
2024-06-07 14:17:45 +0100

    test:GREEN guess_is_correct determines whether guess is present in target word

--- flask-server/src/guess_is_correct.py
+++ flask-server/src/guess_is_correct.py
@@ -1,2 +1,2 @@
-def guess_is_correct():
-    return True
+def guess_is_correct(guess, target_word):
+    return guess in target_word
--- flask-server/tests/test_guess_correct.py
+++ flask-server/tests/test_guess_correct.py
@@ -1,4 +1,7 @@
 from src.guess_is_correct import guess_is_correct
 
 def test_guess_present_in_word():
-    assert guess_is_correct() == True
+    assert guess_is_correct('d', 'word') == True
+
+def test_guess_is_not_present_in_word():
+    assert guess_is_correct('a', 'word') == False
